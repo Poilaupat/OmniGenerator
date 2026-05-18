07eabcf909770f277730a7665d5717e7068e5bb0
Poilaupat <ruben.delapille@gmail.com>
2026-05-18 17:43:38 +0200

    Improvement

--- OmniGenerator.Lib/Autofac/OmniGeneratorModule.cs
+++ OmniGenerator.Lib/Autofac/OmniGeneratorModule.cs
@@ -21,7 +21,6 @@ namespace OmniGenerator.Lib.Autofac
     internal class OmniGeneratorModule : Module
     {
         private readonly IConfiguration _configuration;
-        private static bool _liquidFiltersRegistered = false;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="OmniGeneratorModule"/> class.
@@ -30,21 +29,6 @@ namespace OmniGenerator.Lib.Autofac
         public OmniGeneratorModule(IConfiguration configuration)
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-
-            // Register DotLiquid custom filters once
-            RegisterLiquidFilters();
-        }
-
-        /// <summary>
-        /// Registers custom DotLiquid filters for use in composite field templates.
-        /// This method is called once during module initialization.
-        /// </summary>
-        private static void RegisterLiquidFilters()
-        {
-            if (_liquidFiltersRegistered) return;
-
-            Template.RegisterFilter(typeof(LiquidCustomFilters));
-            _liquidFiltersRegistered = true;
         }
 
         /// <summary>
@@ -53,10 +37,11 @@ namespace OmniGenerator.Lib.Autofac
         /// <param name="builder">The Autofac container builder.</param>
         protected override void Load(ContainerBuilder builder)
         {
+            // Register DotLiquid custom filters
+            Template.RegisterFilter(typeof(LiquidCustomFilters));
+
             // Register Serilog logger into the Autofac container using extension method
             builder.AddSerilog(_configuration);
-
-            // Register Mapperly mapper instance
             builder.RegisterType<FieldMapper>().As<IFieldMapper>().SingleInstance();
 
             // Register core OmniGenerator services and interfaces
